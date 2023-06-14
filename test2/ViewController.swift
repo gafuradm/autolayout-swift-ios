e457a3918b5c206687a8514f9f081e573ca40a01
Damiruly Gafur <sholpandaniel@yandex.ru>
2023-06-14 17:35:40 +0400

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Aleigh Dinara on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
